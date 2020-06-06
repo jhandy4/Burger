04341e5249800396afab47f6fbb1286caf4532b3
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-06-06 01:15:10 -0400

    update routes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -16,26 +16,44 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/", function(req, res) {
-  burgers.insertOne("burger_name", req.body.name, function() {
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+     function(result) {
     // Send back the ID of the new quote
-    res.redirect("/");
+    // res.redirect("/");
+    res.json({id:result.insertId});
   });
 });
 
-router.put("/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.updateOne(
-    {
-      devoured: true
-    },
-    {id: req.params.id}, function() {
-      res.redirect("/"); 
+    {devoured: req.body.devoured}, condition, function(result) {
+      if (result.changedRows === 0) {
+        return res.status(400).end();
+      } else {
+        res.status(200).end();
+      }
+    // {id: req.params.id}, function() {
+    //   res.redirect("/"); 
     });
 });
+    router.delete("/api/burgers/:id", function(req, res) {
+      var condition = "id = " + req.params.id;
+      console.log("condition", condition);
 
+      burger.deleteOne(condition, function(result) {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
+    });
 // Export routes for server.js to use.
 module.exports = router;
